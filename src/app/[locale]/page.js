698dc2c976803207c67d53a5d18f524de3eeffeb
Abhiram Krishna <kabhiram67@gmail.com>
2026-09-18 10:06:25 +0530

    Make pistons bigger, slower, and more subtle

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -796,14 +796,14 @@ export default function LocalizedHomePage({ params }) {
               <div className="piston-marquee-track">
                 {[...Array(20)].map((_, i) => (
                   <div key={`p-r${rowIndex}-${i}`} className="piston-item">
-                    <PistonSvg size={28} />
+                    <PistonSvg size={56} />
                   </div>
                 ))}
               </div>
               <div className="piston-marquee-track" aria-hidden="true">
                 {[...Array(20)].map((_, i) => (
                   <div key={`p-r${rowIndex}-dup-${i}`} className="piston-item">
-                    <PistonSvg size={28} />
+                    <PistonSvg size={56} />
                   </div>
                 ))}
               </div>
